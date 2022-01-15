255e2a1d5d9f1cd15de6450a21463074a31fec09
Leo Omaña <leotangram@gmail.com>
2022-01-15 16:16:19 -0500

    deleting shared module selection & fix refresh history

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,28 +1,28 @@
-const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
+const { merge } = require('webpack-merge')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+const commonConfig = require('./webpack.common')
+const packageJson = require('../package.json')
 
 const devConfig = {
   mode: 'development',
   devServer: {
     port: 8081,
-    historyApiFallback: {
-      index: 'index.html',
-    },
+    historyApiFallback: true
   },
   plugins: [
     new ModuleFederationPlugin({
       name: 'marketing',
       filename: 'remoteEntry.js',
       exposes: {
-        './MarketingApp': './src/bootstrap',
+        './MarketingApp': './src/bootstrap'
       },
+      shared: packageJson.dependencies
     }),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
-};
+      template: './public/index.html'
+    })
+  ]
+}
 
-module.exports = merge(commonConfig, devConfig);
+module.exports = merge(commonConfig, devConfig)
